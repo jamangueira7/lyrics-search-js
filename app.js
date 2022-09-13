742459c6d67f027e5844d2b40a775b422fbf869d
João Mangueira <jamjs7@gmail.com>
2022-09-13 11:48:04 -0300

    busca aparecendo na tela e estilizada

--- app.js
+++ app.js
@@ -5,10 +5,22 @@ const prevAndNextContainer = document.querySelector('#prev-and-next-container');
 
 const apiURL = `https://api.lyrics.ovh`;
 
+const insertSongsIntoPage = async songsInfo => {
+    songsContainer.innerHTML = songsInfo.data
+        .map(song => `
+          <li class="song">
+        <span class="song-artist"><strong>${song.artist.name}</strong> - ${song.title}</span>
+        <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Ver letra</button>
+        </li>
+        `).join('');
+};
+
 const fetchSongs = async term => {
     const response = await fetch(`${apiURL}/suggest/${term}`);
     const data = await response.json();
 
+
+    insertSongsIntoPage(data);
 };
 
 form.addEventListener('submit', event => {
