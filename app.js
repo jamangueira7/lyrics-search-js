c3108f9487912f572ebfad8c601e7c5cef81f57f
João Mangueira <jamjs7@gmail.com>
2022-09-13 12:03:39 -0300

    busca aparecendo na tela e estilizad

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ const prevAndNextContainer = document.querySelector('#prev-and-next-container');
 
 const apiURL = `https://api.lyrics.ovh`;
 
+const getMoreSongs = async url => {
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+    const data = await response.json();
+
+    insertSongsIntoPage(data);
+}
+
 const insertSongsIntoPage = async songsInfo => {
+
+
     songsContainer.innerHTML = songsInfo.data
         .map(song => `
           <li class="song">
@@ -13,6 +22,17 @@ const insertSongsIntoPage = async songsInfo => {
         <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Ver letra</button>
         </li>
         `).join('');
+    
+    if (songsInfo.prev || songsInfo.next) {
+        prevAndNextContainer.innerHTML = `
+            ${ songsInfo.prev ? `<button class="btn" onClick="getMoreSongs('${songsInfo.prev}')">Anteriores</button>` : '' }
+            ${ songsInfo.next ? `<button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Próximas</button>` : '' }
+        `;
+
+        return;
+    }
+
+    prevAndNextContainer.innerHTML = '';
 };
 
 const fetchSongs = async term => {
